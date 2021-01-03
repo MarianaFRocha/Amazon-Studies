2ead04d9ac3ef42cf1e3b0b74b6f26ae70a5d3c5
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2021-01-02 21:37:33 -0300

    Tentativa - Tree: Level Order Traversal

--- Problemas/Binary Tree/src/Solution.java
+++ Problemas/Binary Tree/src/Solution.java
@@ -10,7 +10,6 @@ class Solution {
     */
 
 
-
     public static int numberOfNodes(Node root) {
 
         if (root == null) {
@@ -38,10 +37,29 @@ class Solution {
     }
 
 
+    public static int height2(int t) {
+        return (int) (Math.log(t) / Math.log(2));
+    }
 
-/*    public static int height(int t) {
-        return (int)(Math.log(t) / Math.log(2));
-    }*/
+    public static void levelOrderFolhas(Node root) {
+        if((root != null) && (root.left != null || root.right != null)){
+            if(root.left != null){
+                System.out.print(" " + root.left.data);
+            }
+            if(root.right != null){
+                System.out.print(" " + root.right.data);
+            }
+
+            levelOrderFolhas(root.left);
+            levelOrderFolhas(root.right);
+        }
+    }
+
+
+    public static void levelOrder(Node root) {
+        System.out.print(root.data);
+        levelOrderFolhas(root);
+    }
 
     public static Node insert(Node root, int data) {
         if (root == null) {
@@ -62,6 +80,19 @@ class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }
+
+    /*    *//*Problema Tree: Height of a Binary Tree*//*
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
 
         Node root = null;
         while (t-- > 0) {
@@ -71,5 +102,7 @@ class Solution {
         scan.close();
         int height = height(root);
         System.out.println(height);
-    }
+    }*/
+
+
 }
